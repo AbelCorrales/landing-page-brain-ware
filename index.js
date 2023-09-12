a388d41e576aa9c46bd2d8d062853f018e14a92b
abelcorrales34 <136219207+abelcorrales34@users.noreply.github.com>
2023-09-11 22:36:40 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var folder = path.join(__dirname, 'public');
+app.use(express.json());
+app.use(express.static(folder));
+
+app.listen(5000, function(){
+  console.log("Servidor arriba en 5000");
+});
